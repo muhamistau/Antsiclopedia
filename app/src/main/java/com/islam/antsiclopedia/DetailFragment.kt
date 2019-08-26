6a39a41f7aec2f5af6fa813b11f035280affa874
muhamistau <islam.taufikurahman@gmail.com>
2019-08-26 14:50:48 +0700

    Update Navigation Arguments Data Passing

--- app/src/main/java/com/islam/antsiclopedia/DetailFragment.kt
+++ app/src/main/java/com/islam/antsiclopedia/DetailFragment.kt
@@ -6,8 +6,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_detail.*
+import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import com.islam.antsiclopedia.entity.Ant
+import kotlinx.android.synthetic.main.content.*
+
 
 class DetailFragment : Fragment() {
+
+    lateinit var ant: Ant
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -17,5 +25,16 @@ class DetailFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_detail, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        (activity as AppCompatActivity).setSupportActionBar(toolbar)
+        ant = DetailFragmentArgs.fromBundle(arguments!!).Ant
 
+        toolbar.title = ant.name
+
+        Glide.with(this).load(ant.imageUrl).into(header)
+
+        latinName.text = ant.latin
+        description.text = ant.description
+    }
 }
--- app/src/main/java/com/islam/antsiclopedia/entity/Ant.kt
+++ app/src/main/java/com/islam/antsiclopedia/entity/Ant.kt
@@ -1,8 +1,14 @@
 package com.islam.antsiclopedia.entity
 
+import android.annotation.SuppressLint
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+@SuppressLint("ParcelCreator")
 data class Ant(
     val name: String,
     val latin: String,
     val description: String,
     val imageUrl: String
-)
+) : Parcelable
